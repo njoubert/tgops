693731b55145e6ece6c320ecab8bc913aa8bf71f
Niels Joubert <njoubert@gmail.com>
2019-07-11 13:50:23 -0700

    Check for Ubuntu 18.04'

--- provisioning/otto/provision_tmux.sh
+++ provisioning/otto/provision_tmux.sh
@@ -15,6 +15,13 @@ set -e
 # echo every command, we're being verbose
 set -x
 
+# ensure Ubuntu 18.04
+if ! lsb_release -a 2>/dev/null | grep -q "Ubuntu 18.04"; then
+    echo "Compatible only with Ubuntu 18.04 LTS"
+    exit
+fi
+
+
 # only run as root, we're installing stuff
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
