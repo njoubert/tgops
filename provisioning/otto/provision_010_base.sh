b1cc9a8d7509fdfdcd9877d36173c4d0ad77be23
Niels Joubert <njoubert@gmail.com>
2019-07-13 10:47:13 -0700

    hard symlink bash

--- provisioning/otto/provision_010_base.sh
+++ provisioning/otto/provision_010_base.sh
@@ -15,26 +15,8 @@
 ###### MEAT AND POTATOES ######
 
 # Link .bashrc and .bach_profile dotfiles
-echo "+ Symlinking bashrc and bash_profile"
-BASHRC_INCLUDE="$(cat <<EOF
-$SCRIPT_SIG
-if [ -f $PROVISION/dotfiles/bashrc.sh ]; then
-        source $PROVISION/dotfiles/bashrc.sh
-fi
-### END
-EOF
-)"
-echo -e "$BASHRC_INCLUDE" >> $HOME/.bashrc
-
-BASHPROFILE_INCLUDE="$(cat <<EOF
-$SCRIPT_SIG
-if [ -f $PROVISION/dotfiles/bash_profile.sh ]; then
-        source $PROVISION/dotfiles/bash_profile.sh
-fi
-### END
-EOF
-)"
-echo -e "$BASHPROFILE_INCLUDE" >> $HOME/.bash_profile
+ln -s $PROVISION/dotfiles/bash_profile.sh $HOME/.bash_profile
+ln -s $PROVISION/dotfiles/bashrc.sh $HOME/.bashrc
 
 # Install basic editors
 echo "+ Installing emacs, vim, git"
